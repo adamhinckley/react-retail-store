50a72cb85824f6f853917b86d9cfe2351e85a2cf
adamhinckley <adamhinckley@mac.com>
2018-12-21 13:05:08 -0700

    sets up beginning of nav and components

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Page extends Component {
+  render() {
+    return (
+      <div>
+        <p>Het I'm the page component</p>
+      </div>
+    );
+  }
+}
