4c05a69ace9b649afe8f46f549b3b5888f093b38
adamhinckley <adamhinckley@mac.com>
2018-12-21 13:11:10 -0700

    adds header component

---
+++ sick-fits/frontend/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  <div>
+    <div className="bar">
+      <a href="Fake Store" />
+    </div>
+    <div className="sub-bar">
+      <p>Search</p>
+    </div>
+    <div>Cart</div>
+  </div>;
+};
+
+export default Header;
--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Header from "../components/Header";
 
 export default class Page extends Component {
   render() {
     return (
       <div>
-        <p>Het I'm the page component</p>
+        <Header />
+        {this.props.children}
       </div>
     );
   }
