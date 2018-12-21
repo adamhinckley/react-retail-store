62187d6f5531d34055e25e37862b39f12ba2afcb
adamhinckley <adamhinckley@mac.com>
2018-12-21 13:16:56 -0700

    adds Nav component

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,13 +1,15 @@
 const Header = () => {
-  <div>
-    <div className="bar">
-      <a href="Fake Store" />
+  return (
+    <div>
+      <div className="bar">
+        <a href="Fake Store" />
+      </div>
+      <div className="sub-bar">
+        <p>Search</p>
+      </div>
+      <div>Cart</div>
     </div>
-    <div className="sub-bar">
-      <p>Search</p>
-    </div>
-    <div>Cart</div>
-  </div>;
+  );
 };
 
 export default Header;
