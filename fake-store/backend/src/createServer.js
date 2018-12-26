d2ac0d388d0d985bb4f46ec428265707e08d0ddf
adamhinckley <adamhinckley@mac.com>
2018-12-26 14:42:42 -0700

    local server setup and running

--- fake-store/backend/src/createServer.js
+++ fake-store/backend/src/createServer.js
@@ -1,18 +1,18 @@
-const { graphQLServer } = require("graphql-yoga");
+const { GraphQLServer } = require("graphql-yoga");
 const Mutation = require("./resolvers/Mutation");
 const Query = require("./resolvers/Query");
 const db = require("./db");
 
-// Create GraphQL Yoga Server
+// Create the GraphQL Yoga Server
 
 function createServer() {
-  return new graphQLServer({
+  return new GraphQLServer({
     typeDefs: "src/schema.graphql",
     resolvers: {
       Mutation,
       Query
     },
-    resolverValidateOptions: {
+    resolverValidationOptions: {
       requireResolversForResolveType: false
     },
     context: req => ({ ...req, db })
--- fake-store/backend/src/db.js
+++ fake-store/backend/src/db.js
@@ -2,8 +2,8 @@
 
 const { Prisma } = require("prisma-binding");
 
-const db = newPrisma({
-  typeDefs: "/src/generated/prisma.graphql",
+const db = new Prisma({
+  typeDefs: "src/generated/prisma.graphql",
   endpoint: process.env.PRISMA_ENDPOINT,
   secret: process.env.PRISMA_SECRET,
   debug: false
